9ddde164cd1ba2f76f2a0d5d4f9f801aec59cd8d
typesafety <ailoodee@gmail.com>
2025-12-03 22:52:50 +0100

    Add solution template file

---
+++ src/solutions/template.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from pprint import pprint, pformat  # noqa: F401
+
+
+def solve_part1(puzzle_input: str) -> str:
+    return puzzle_input
+
+
+def solve_part2(puzzle_input: str) -> str:
+    return puzzle_input
