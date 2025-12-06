c8d4a4584389ad14a4ab6817be04a4bd44f545e8
typesafety <ailoodee@gmail.com>
2025-12-06 17:22:32 +0100

    Add day 5 part 2 solution

--- src/solutions/day05.py
+++ src/solutions/day05.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import copy
 from dataclasses import dataclass
 from pprint import pprint, pformat  # noqa: F401
 from typing import Self
@@ -17,15 +18,31 @@ def solve_part1(puzzle_input: str) -> str:
 
 
 def solve_part2(puzzle_input: str) -> str:
-    return puzzle_input
+    ranges, _ = parse_input(puzzle_input)
+    merged = merge_ranges(ranges)
+
+    return str(sum(1 + r.end - r.start for r in merged))
 
 
-def parse_input(puzzle_input: str) -> tuple[set[Range], set[int]]:
-    [ranges, ids] = puzzle_input.split("\n\n")
-    range_lines = ranges.strip().splitlines()
-    id_lines = ids.strip().splitlines()
+def merge_ranges(ranges: set[Range]) -> set[Range]:
+    new: set[Range] = set()
 
-    return {Range.parse(line) for line in range_lines}, {int(ident) for ident in id_lines}
+    for r in ranges:
+        new = add_range(r, new)
+
+    return new
+
+
+def add_range(new_range: Range, ranges: set[Range]) -> set[Range]:
+    new = copy.copy(ranges)
+
+    for r in ranges:
+        if merged := r.merge(new_range):
+            new.remove(r)
+            return add_range(merged, new)
+
+    new.add(new_range)
+    return new
 
 
 @dataclass(frozen=True)
@@ -40,3 +57,25 @@ class Range:
 
     def contains(self, ident: int) -> bool:
         return ident >= self.start and ident <= self.end
+
+    def intersects(self, r: Range) -> bool:
+        return (
+            (r.start < self.start and r.end > self.end)
+            or self.contains(r.start)
+            or self.contains(r.end)
+        )
+
+    def merge(self, r: Range) -> Range | None:
+        return (
+            Range(start=min(self.start, r.start), end=max(self.end, r.end))
+            if self.intersects(r)
+            else None
+        )
+
+
+def parse_input(puzzle_input: str) -> tuple[set[Range], set[int]]:
+    [ranges, ids] = puzzle_input.split("\n\n")
+    range_lines = ranges.strip().splitlines()
+    id_lines = ids.strip().splitlines()
+
+    return {Range.parse(line) for line in range_lines}, {int(ident) for ident in id_lines}
