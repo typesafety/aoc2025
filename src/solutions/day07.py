ebdbd14d6a349782f258e0201fd0bc3e1d81d52f
typesafety <ailoodee@gmail.com>
2025-12-07 20:09:41 +0100

    Add day 7 part 2 solution

--- src/solutions/day07.py
+++ src/solutions/day07.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from pprint import pprint, pformat  # noqa: F401
 from typing import Dict, Set
+
+from src.utils import unwrap
 
 
 def solve_part1(puzzle_input: str) -> str:
@@ -16,7 +17,12 @@ def solve_part1(puzzle_input: str) -> str:
 
 
 def solve_part2(puzzle_input: str) -> str:
-    return puzzle_input
+    grid = parse_grid(puzzle_input)
+    first = unwrap(next_down(grid, grid.start))
+    index: SickIndex = {}
+    total = sick_index(first, grid, index)
+
+    return str(total)
 
 
 @dataclass
@@ -32,6 +38,30 @@ class Coord:
 
 
 type Index = Dict[Coord, Set[Coord]]
+type SickIndex = Dict[Coord, int]
+
+
+def sick_index(coord: Coord, grid: Grid, cache: SickIndex) -> int:
+    if coord in cache:
+        return cache[coord]
+
+    child_coords = get_child_coords(coord, grid)
+    children_timelines = sum(sick_index(c, grid, cache) for c in child_coords)
+
+    timelines = children_timelines + 2 - len(child_coords)
+    cache[coord] = timelines
+
+    return timelines
+
+
+def get_child_coords(coord: Coord, grid: Grid) -> set[Coord]:
+    return {
+        c
+        for c in {
+            next_down(grid, Coord(child_x, coord.y)) for child_x in (coord.x - 1, coord.x + 1)
+        }
+        if c is not None
+    }
 
 
 def next_down(g: Grid, from_coord: Coord) -> Coord | None:
